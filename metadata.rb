2b4cd93d2c1c329cf530b2e9e715f366611f1333
Arvid Björkström <arvid@bjorkstrom.se>
2014-09-02 17:47:15 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'compass'
+maintainer       'Arvid Björkström'
+maintainer_email 'arvid@bjorkstrom.se'
+license          'MIT'
+description      'Installs/Configures compass'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
