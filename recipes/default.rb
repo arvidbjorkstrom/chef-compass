0d383f81c8975b61416c6d9dc7a48e551556f2ee
Arvid Björkström <arvid@bjorkstrom.se>
2014-09-03 11:27:10 +0200

    Install gems

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,3 +2,22 @@
 # Cookbook Name:: compass
 # Recipe:: default
 #
+
+package 'rubygems'
+
+execute 'Update Gems' do
+  command 'gem update --system'
+  action :run
+end
+
+execute 'Install Sass' do
+  command 'gem install sass'
+  action :run
+  not_if 'which sass'
+end
+
+execute 'Install Compass' do
+  command 'gem install compass'
+  action :run
+  not_if 'which compass'
+end
