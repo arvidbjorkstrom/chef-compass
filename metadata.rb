b3695228948f4dcdf46b655548ab51bce259935b
Arvid Björkström <arvid@bjorkstrom.se>
2014-12-22 00:18:02 +0100

    Remove non US-ASCII character

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name             'compass'
-maintainer       'Arvid Björkström'
+maintainer       'Arvid Bjorkstrom'
 maintainer_email 'arvid@bjorkstrom.se'
 license          'MIT'
 description      'Installs/Configures compass'
